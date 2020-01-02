2a45d0699b2b9df26500d7f3d9fc1d64ac1174de
kdnakt <a.kid.1985@gmail.com>
2020-01-02 17:50:05 +0900

    use method-local inner class

--- chapter02/b_inner_classes/src/com.kdnakt/com/kdnakt/java11/Vehicle.java
+++ chapter02/b_inner_classes/src/com.kdnakt/com/kdnakt/java11/Vehicle.java
@@ -2,31 +2,31 @@ package com.kdnakt.java11;
 
 public class Vehicle {
     private int weightPounds;
-    private Engine engine;
+    private int horsePower;
 
     public Vehicle(int weightPounds, int horsePower) {
         this.weightPounds = weightPounds;
-        this.engine = new Engine(horsePower);
-    }
-
-    public double getSpeedMph(double timeSec) {
-        return this.engine.getSpeedMph(timeSec);
+        this.horsePower = horsePower;
     }
 
     private int getWeightPounds() {
         return weightPounds;
     }
 
-    private class Engine {
-        private int horsePower;
+    public double getSpeedMph(double timeSec) {
+        class Engine {
+            private int horsePower;
 
-        private Engine(int horsePower) {
-            this.horsePower = horsePower;
-        }
+            private Engine(int horsePower) {
+                this.horsePower = horsePower;
+            }
 
-        private double getSpeedMph(double timeSec) {
-            double v = 2.0 * this.horsePower * 746 * timeSec * 32.17 / getWeightPounds();
-            return Math.round(Math.sqrt(v) * 0.68);
+            private double getSpeedMph(double timeSec) {
+                double v = 2.0 * this.horsePower * 746 * timeSec * 32.17 / getWeightPounds();
+                return Math.round(Math.sqrt(v) * 0.68);
+            }
         }
+        Engine engine = new Engine(this.horsePower);
+        return engine.getSpeedMph(timeSec);
     }
 }
