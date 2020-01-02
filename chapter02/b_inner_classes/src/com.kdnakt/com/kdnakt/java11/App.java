10611980be22935e47f65ce98e6a99e9378e493f
kdnakt <a.kid.1985@gmail.com>
2020-01-02 18:02:47 +0900

    used annonymous inner class

--- chapter02/b_inner_classes/src/com.kdnakt/com/kdnakt/java11/App.java
+++ chapter02/b_inner_classes/src/com.kdnakt/com/kdnakt/java11/App.java
@@ -5,7 +5,11 @@ public class App {
         double timeSec = 10.0;
         int horsePower = 246;
         int vehicleWeight = 4000;
-        Vehicle vehicle = new Vehicle(vehicleWeight, horsePower);
+        Vehicle vehicle = new Vehicle(vehicleWeight, horsePower) {
+            public double getSpeedMph(double timeSec) {
+                return -1.0d;
+            }
+        };
         System.out.println("Vehicle speed (" + timeSec + " sec)=" + vehicle.getSpeedMph(timeSec) + " mph");
     }
 }
