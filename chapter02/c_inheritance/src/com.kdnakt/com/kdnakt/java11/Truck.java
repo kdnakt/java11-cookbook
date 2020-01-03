5f037ee0a2bb81ad6d976a7a084bd5a33208767c
kdnakt <a.kid.1985@gmail.com>
2020-01-03 10:41:13 +0900

    add getSpeedMph to Truck class

--- chapter02/c_inheritance/src/com.kdnakt/com/kdnakt/java11/Truck.java
+++ chapter02/c_inheritance/src/com.kdnakt/com/kdnakt/java11/Truck.java
@@ -11,4 +11,10 @@ public class Truck extends Vehicle {
     public int getPayload() {
         return this.payload;
     }
+
+    public double getSpeedMph(double timeSec) {
+        int weight = this.weightPounds + this.payload;
+        double v = 2.0 * this.horsePower * 746 * timeSec * 32.17 / weight;
+        return Math.round(Math.sqrt(v) * 0.68);
+    }
 }
